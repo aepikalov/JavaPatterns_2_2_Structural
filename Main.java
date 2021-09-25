25300ed3c2b6b504f734a3f45d9f0a43ec53075a
Artem Pikalov <aepikalov@gmail.com>
2021-09-25 23:47:11 +0400

    JavaPatterns_2_2_Structural

---
+++ Main.java
@@ -0,0 +1,9 @@
+package JavaPatterns_2_2_Structural;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("101", "111"));
+        System.out.println(bins.mult("101", "111"));
+    }
+}
